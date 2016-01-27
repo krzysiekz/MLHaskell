74ff966f8f2434647c450c1e230cbd4a13d6916c
krzysiek <krzysiekcrpgfan@gmail.com>
2016-01-27 21:48:58 +0100

    implemented regression gradient descent

--- src/ML/MultipleRegression.hs
+++ src/ML/MultipleRegression.hs
@@ -5,3 +5,21 @@ import Utils.Common
 computeFeatureDerivative :: [Double] -> [Double] -> Double
 computeFeatureDerivative errors feature = 2 * dotProductRow errors feature
 
+regressionGradientDescent :: [[Double]] -> [Double] -> [Double] -> Double -> Double -> [Double]
+regressionGradientDescent [] _ _ _ _ = error "Empty feature matrix"
+regressionGradientDescent _ [] _ _ _ = error "Empty output array"
+regressionGradientDescent _ _ [] _ _ = error "Empty weights array"
+regressionGradientDescent featureMatrix output initialWeights stepSize tolerance
+    | gradientMagnitude < tolerance = updatedWeights
+    | otherwise = regressionGradientDescent featureMatrix output updatedWeights stepSize tolerance
+    where errors = zipWith (-) (predictOutput featureMatrix initialWeights) output
+          (updatedWeights, gradientSumSquares) = updateWeights featureMatrix errors initialWeights [] 0 stepSize 0
+          gradientMagnitude = sqrt gradientSumSquares
+
+updateWeights :: [[Double]] -> [Double] -> [Double] -> [Double] -> Double -> Double -> Int -> ([Double], Double)
+updateWeights _ _ [] updatedWeights gradientSumSquares _ _= (updatedWeights, gradientSumSquares)
+updateWeights featureMatrix errors weights@(x:xs) updatedWeights gradientSumSquares stepSize index =
+    updateWeights featureMatrix errors xs (updatedWeights ++ [newWeightValue]) newGradientSumSquares stepSize (index+1)
+    where derivative = computeFeatureDerivative errors $ getColumn featureMatrix index
+          newGradientSumSquares = gradientSumSquares + (derivative * derivative)
+          newWeightValue = x - (derivative * stepSize)
--- src/Main2.hs
+++ src/Main2.hs
@@ -14,23 +14,35 @@ testFileName = "resources/kc_house_test_data.csv"
 
 main =
     do
-       training <- readFile dataFileName
-       case parseCSV training of
+       allDataRaw <- readFile dataFileName
+       case parseCSV allDataRaw of
             Left err -> do
                          putStrLn "Error parsing input:"
                          print err
-            Right trainingData -> do
-                    putStrLn ("Read " ++ show (length trainingData) ++ " lines")
-                    let (featuresMatrix, outputArray) = getMatrixData (tail trainingData) [5] 2
-                    print $ show $ head featuresMatrix
-                    print $ show $ head outputArray
-                    let weights = replicate 2 1
-                    let testPredictions = predictOutput featuresMatrix weights
-                    print $ head testPredictions
-                    print $ testPredictions !! 1
-                    let weights2 = replicate 2 0
-                    let testPredictions2 = predictOutput featuresMatrix weights2
-                    let errors = zipWith (-) testPredictions2 outputArray
-                    let feature = getColumn featuresMatrix 0
-                    let derivative = computeFeatureDerivative errors feature
-                    print derivative
+            Right allData -> do
+--                     putStrLn ("Read " ++ show (length allData) ++ " lines")
+--                     let (featuresMatrix, outputArray) = getMatrixData (tail allData) [5] 2
+--                     print $ show $ head featuresMatrix
+--                     print $ show $ head outputArray
+--                     let weights = replicate 2 1
+--                     let testPredictions = predictOutput featuresMatrix weights
+--                     print $ head testPredictions
+--                     print $ testPredictions !! 1
+--                     let weights2 = replicate 2 0
+--                     let testPredictions2 = predictOutput featuresMatrix weights2
+--                     let errors = zipWith (-) testPredictions2 outputArray
+--                     let feature = getColumn featuresMatrix 0
+--                     let derivative = computeFeatureDerivative errors feature
+--                     print derivative
+                    trainingDataRaw <- readFile trainingFileName
+                    case parseCSV trainingDataRaw of
+                        Left err -> do
+                                     putStrLn "Error parsing input:"
+                                     print err
+                        Right trainingData -> do
+                               let (featuresMatrixTraining, outputArrayTraining) = getMatrixData (tail trainingData) [5] 2
+                               let trainingInitialWeights = [-47000, 1]
+                               let trainingStepSize = 7e-12
+                               let trainingTolerance = 2.5e7
+                               let calculatedTrainingWeights = regressionGradientDescent featuresMatrixTraining outputArrayTraining trainingInitialWeights trainingStepSize trainingTolerance
+                               print calculatedTrainingWeights
