b181bf1bb6e61f8dcc1e441de8fe6bf85203e210
krzysiek <krzysiekcrpgfan@gmail.com>
2016-01-27 22:32:00 +0100

    multi regression tests

--- src/Main2.hs
+++ src/Main2.hs
@@ -20,20 +20,20 @@ main =
                          putStrLn "Error parsing input:"
                          print err
             Right allData -> do
---                     putStrLn ("Read " ++ show (length allData) ++ " lines")
---                     let (featuresMatrix, outputArray) = getMatrixData (tail allData) [5] 2
---                     print $ show $ head featuresMatrix
---                     print $ show $ head outputArray
---                     let weights = replicate 2 1
---                     let testPredictions = predictOutput featuresMatrix weights
---                     print $ head testPredictions
---                     print $ testPredictions !! 1
---                     let weights2 = replicate 2 0
---                     let testPredictions2 = predictOutput featuresMatrix weights2
---                     let errors = zipWith (-) testPredictions2 outputArray
---                     let feature = getColumn featuresMatrix 0
---                     let derivative = computeFeatureDerivative errors feature
---                     print derivative
+                    putStrLn ("Read " ++ show (length allData) ++ " lines")
+                    let (featuresMatrix, outputArray) = getMatrixData (tail allData) [5] 2
+                    print $ show $ head featuresMatrix
+                    print $ show $ head outputArray
+                    let weights = replicate 2 1
+                    let testPredictions = predictOutput featuresMatrix weights
+                    print $ head testPredictions
+                    print $ testPredictions !! 1
+                    let weights2 = replicate 2 0
+                    let testPredictions2 = predictOutput featuresMatrix weights2
+                    let errors = zipWith (-) testPredictions2 outputArray
+                    let feature = getColumn featuresMatrix 0
+                    let derivative = computeFeatureDerivative errors feature
+                    print derivative
                     trainingDataRaw <- readFile trainingFileName
                     case parseCSV trainingDataRaw of
                         Left err -> do
@@ -46,3 +46,21 @@ main =
                                let trainingTolerance = 2.5e7
                                let calculatedTrainingWeights = regressionGradientDescent featuresMatrixTraining outputArrayTraining trainingInitialWeights trainingStepSize trainingTolerance
                                print calculatedTrainingWeights
+                               testDataRaw <- readFile testFileName
+                               case parseCSV testDataRaw of
+                                Left err -> do
+                                    putStrLn "Error parsing input:"
+                                    print err
+                                Right testData -> do
+                                    let (featuresMatrixTest, outputArrayTest) = getMatrixData (tail testData) [5] 2
+                                    let predictionsTest = predictOutput featuresMatrixTest calculatedTrainingWeights
+                                    print predictionsTest
+                                    let residuals = zipWith (-) outputArrayTest predictionsTest
+                                    let rssTestData = sum $ map (\a -> a * a) residuals
+                                    print rssTestData
+                                    let (featuresMatrixTrainingMulti, outputArrayTrainingMulti) = getMatrixData (tail trainingData) [5,19] 2
+                                    let trainingInitialWeightsMulti = [-100000, 1, 1]
+                                    let trainingStepSizeMulti = 4e-12
+                                    let trainingToleranceMulti = 1e9
+                                    let calculatedTrainingWeightsMulti = regressionGradientDescent featuresMatrixTrainingMulti outputArrayTrainingMulti trainingInitialWeightsMulti trainingStepSizeMulti trainingToleranceMulti
+                                    print calculatedTrainingWeightsMulti
